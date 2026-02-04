e97ae1f61e4bbfdb7dc7d3a3b938b2886cf4353a
archigeosn-tech <archigeosn@gmail.com>
2026-02-04 12:16:25 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,66 @@
+import streamlit as st
+st.subheader("Hydraulicien / Geomaticien")
+
+st.markdown("""je suis Techicien Supérieur en suivie Technique et Gestion des Ouvrages Hydrauliques ,doter de compétences en Diagnostique et réhabilitation des réseaux AEP, réalisation des aouvraces hydrauliques en béton armé mais aussi en réalisation des ouvrages de Stockages.Je suis aussi specialiste en géomatique avec la Maîtrise des Outils SIG Systèmes d'Information Géographique""")
+
+st.header("📚Formations")
+col1,col2 = st.columns(2)
+with col1:
+        st.write("""* Diplôme de Technicien Supérieur en Suivie Technique et Gestion des Ouvrages Hydrauliques /ISEP-Thies""")
+with col2:
+         st.write("""* Brevet de Technicien Supérieur en Geomatique /CEDT Le G15""")
+        
+
+st.header("🛠️Competences")
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader("Geomatique")
+    st.write("* ✅Maîtrise des Outils SIG Systèmes d'Information Géographique")
+    st.write("* ✅Acquisition et traitement des données sur ArcMap et Qgis")
+    st.write("* ✅Etude Topographique")
+    st.write("* ✅Lever Topographique avec Station Total")
+    st.write("* ✅Collecte de données avec Mobil Topo")
+    st.write("* ✅Utilisation des Outils de Travail Colaboratifs")
+    st.write("* ✅Modélisation Batiment 2D sur AutoCAD")
+    st.write("* ✅Modélisation Batiment 3D sur Skeshup")
+    st.write("* ✅Anglais Technique")
+with col2:
+    st.subheader("Hydraulique")
+    st.write("* ✅Dimensionnement de réseaux et d'ouvrages hydraulique")
+    st.write("* ✅Hydraulique à surface libre")
+    st.write("* ✅Qualité de l'eau et traitement")
+    st.write("* ✅Lecture et réalisation de plans DAO")
+    st.write("* ✅Études géotechniques")
+    st.write("* ✅Connaissance des Matériaux de construction")
+    st.write("* ✅Etude Topographique")
+    st.write("* ✅Etude Geophysique")
+    st.write("* ✅Modélisation DAO sur Atocad")
+    st.write("* ✅Modélisation DAO sur RSA")
+    st.write("* ✅Lever Topographique avec Station Total et Niveau Topique")
+
+
+st.header("🏗️Experiences")
+st.markdown("""* 🛠️STAGE EN HYDRAULIQUE
+    * Poste : Technicien Superieur en Hydraulique Urbaine / Agricole
+    * Entreprise: SOLSO HYDROBAT
+    * 📌Thiès""")
+st.write(" ▶️Etude reseau AEP")
+st.write(" ▶️Dimensionnement reseau AEP")
+st.write(" ▶️Gestion des ressources en eau")
+st.write(" ▶️Diagnostique des ouvrages")
+st.write(" ▶️Reduction des fuites sur un reseau AEP")
+st.write(" ▶️Optimisation de la distribution de leau")
+st.write(" ▶️Realisation ouvrage de captage: Forage 100m")
+with st.sidebar:
+    st.subheader("📌Fallou DIATTA")
+    st.subheader("🏠Adresse")
+    st.write("Dakar")
+    st.subheader("📩email")
+    st.write("archigeosn@gmail.com")
+    st.subheader("📞Contact")
+    st.write(772389968)
+
+
+    
+
+
